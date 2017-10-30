efe3f92042891fd198eb034c1ecb2c5d36c2162a
Rafal <rafal.sladek@gmail.com>
2017-10-30 09:39:00 +0100

    changed pool name

--- app/globals.js
+++ app/globals.js
@@ -4,8 +4,8 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
+		pool_name: "XMRCrypto-Pool.net",
+		api_url : 'https://api.xmr.crypto-pool.net',
 		api_refresh_interval: 5000,
 		app_update_interval: 5*60000
 	};
